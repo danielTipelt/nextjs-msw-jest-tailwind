dc6161c75094881044e3c3f43a27317ef51c4d42
Daniel Tipelt <daniel.tipelt@gmail.com>
2022-07-01 21:25:28 +0200

    Install tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Layout } from "../components/Layout";
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      <h1 className="text-3xl">HEADER</h1>
+    </Layout>
+  );
+};
+
+export default Home;
